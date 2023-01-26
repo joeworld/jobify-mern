e94468e401e5d668644e982ccfc68cc24c196abd
Ojo Oluwaseun Joseph <iamjoestack@gmail.com>
2023-01-26 16:06:09 +0100

    Components and some pages

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,22 +1,24 @@
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import Wrapper from "../assets/wrappers/LandingPage";
+import {Logo} from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-        <main>
+        <Wrapper>
             <nav>
-                <img src={logo} alt='jobify' className='logo' />
+                <Logo />
             </nav>
             <div className='container page'>
                 {/* info */}
                 <div className='info'>
                     <h1>job <span>tracking</span> app</h1>
                     <p>I'm baby venmo taxidermy kickstarter kombucha disrupt messenger bag, authentic cred food truck normcore kale chips tumblr semiotics. Asymmetrical hot chicken keffiyeh kickstarter. Leggings put a bird on it scenester, dreamcatcher occupy kitsch chartreuse church-key chicharrones. Whatever slow-carb celiac humblebrag locavore.</p>
-                    <button className='btn btn-hero'>Login/Register</button>
+                    <Link to='/register' className='btn btn-hero'>Login/Register</Link>
                 </div>
                 <img src={main} alt='job hunt' className='img main-img' />
             </div>
-        </main>
+        </Wrapper>
     )
 }
 
